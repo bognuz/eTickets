c37ae087955ee2c9736969297d3bfde12173f29b
Bogdan Nuzhnyy <nuzhnyy@live.dk>
2023-08-31 11:48:41 +0200

    Adding Producers controller with async

---
+++ eTickets/Controllers/ProducersController.cs
@@ -0,0 +1,21 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Controllers
+{
+    public class ProducersController : Controller
+    {
+        private readonly AppDbContext _Context;
+
+        public ProducersController(AppDbContext context)
+        {
+           _Context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allProducers = await _Context.Producers.ToListAsync();
+            return View();
+        }
+    }
+}
